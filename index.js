6d0c7931ce0c6e5aa61e5386a43927c99b22e680
Arima <55054011+Arimsingle@users.noreply.github.com>
2020-02-26 22:26:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.post('/admin', urlencodedParser, (req, res) => {
     session = req.session
     session.email = req.body.email
     password = req.body.password
-    if (session.email == '' && password == '240311' || password == '') {
+    console.log(password,session.email)
+    if (session.email.toString() == '' || password.toString() == '' ) {
         res.send(`<html>
         <head>
         <title>Admin</title>
@@ -31,8 +32,23 @@ app.post('/admin', urlencodedParser, (req, res) => {
         </body>
         </html>`)
     }
-    else {
+    else if(password.toString() == '240311')  {
         res.render('user', { email: req.session.email })
+    }
+    else{
+        res.send(`<html>
+        <head>
+        <title>Admin</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        </head>
+        <body>
+        <h1 style="align:center;">
+        Please Login Frist
+        </h1>
+        <a href="/" class="btn btn-primary">Login</a>
+        </body>
+        </html>`)
     }
 });
 app.get('/logout', (req, res) => {
